a316ceee7163da15041d408b3a36fbcee2ef0902
Parth Krishna <parthkrishna1611@gmail.com>
2019-02-05 00:27:35 +0530

    Created functions using glob, sys and docopt

--- poodle.py
+++ poodle.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
-
+import sys
 from sys import *
+from docopt import docopt
+from glob import glob
 
 
-def open_file(filename):
-    source_code = open(filename, 'r').read()
-    #print(source_code)
-    return source_code
+usage = '''
+
+Poodle Programming Language
+
+Usage:
+    ./poodle.py <filename>.poo
+
+'''
 
 
-def run():
-    data = open_file(argv[1])
+def open_file(source):
+    if source in glob("*.lang"):
+        source_code = open(source, 'r').read()
+        return source_code
+    else:
+        return "ERROR: "+source+" is not a valid file. "
 
-run()
+
+if(len(sys.argv) == 1):
+    args = docopt(usage)
+    print(args)
+
+else:
+    source = argv[1]
+    data = open_file(source)
+    print(data)
+
+
+
