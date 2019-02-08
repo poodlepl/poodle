f99eeb6e0b6b9ee0634e0b907c8c05b3807fe293
kam-pragati <33838284+kam-pragati@users.noreply.github.com>
2019-02-08 12:04:21 +0530

    added code for comments

--- lexer.py
+++ lexer.py
@@ -15,14 +15,39 @@ def lex(filecontents):
 
     tok = ""
     '''
-    words = filecontents.split()
-    print(words)
+    
+    switch={
+		"??" : "Comment",
+		".5" : "For loop",
+		"wrap" : "keyword",
+		"as" : "keyword",
+		"let" : "keyword",
+		"pp" : "incrementer"
+	}
+	num=[0,1,2,3,4,5,6,7,8,9]
+	data=switch.keys()
+	words = filecontents.split('\n')
+	print(words)
+	for w in words:
+		c=0
+		tokens=w.split(" ")
+		for tok in tokens:
+			#print(tok)
+			if c==1:
+				continue
+			elif tok in data:
+				print("Token:",tok,"  Category:",switch[tok])
+				if tok=='??':
+					print("Comment : ",w[2:])
+					c=1
+
+    
     '''
     iterate through em all and process it.
     take raw string
     if there is extra char, check. isAlpha() and all.
     '''
-
+		
 
 #print(keywords)
 #print(relop)
