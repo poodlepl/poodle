5128e1e1312cdcec496f4e15ad6ccd80847a4457
Parth Krishna <parthkrishna1611@gmail.com>
2019-02-10 23:42:40 +0530

    Added version support using docopt

--- poodle.py
+++ poodle.py
@@ -11,27 +11,44 @@ usage = '''
 Poodle Programming Language
 
 Usage:
-    ./poodle.py <filename>.poo
+    ./poodle.py --version
+    ./poodle.py --source <filename>
 
 '''
 
+
+flag = 2
 
 def open_file(source):
     if source in glob("lang/*.lang"):
         source_code = open(source, 'r').read()
         return source_code
     else:
-        return "ERROR: "+source+" is not a valid file. "
+        flag = 1
+        return flag
+
+'''
+
+#EARLIER
+
+# used len(sys.argv) to check file name.
+# shifted to docopts.args for better support
+
+#if(len(sys.argv) == 1):
+#    print(args)
+'''
 
 
-if(len(sys.argv) == 1):
-    args = docopt(usage)
-    print(args)
+args = docopt(usage)
+if args['--version']:
+    print("Poodle Programming Language V1.0")
 
-else:
-    source = argv[1]
-    data = open_file(source)
-    #print(data)
-    lex(data)
+elif args['--source']:
+    source = args['<filename>']
+    try:
+        data = open_file(source)
+        lex(data)
+    except AttributeError:
+        print("ERROR: "+source+" is not a valid file. ")
 
 
