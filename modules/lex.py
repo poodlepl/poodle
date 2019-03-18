c200a3b25357619db680c94a3fd12f101ec89005
Parth Krishna <thenerdsuperuser@gmail.com>
2019-03-19 04:36:54 +0530

    fixed modules->lex.py

--- modules/lex.py
+++ modules/lex.py
@@ -9,21 +9,6 @@ from modules.remove_comments import remove_comments
 pp = pprint.PrettyPrinter(indent = 4)
 DIGITS = '0123456789'
 
-class Error:
-    def __init__(self, pos_start, pos_end, error_name, details):
-        self.pos_start = pos_start
-        self.pos_end = pos_end
-        self.error_name = error_name
-        self.details = details
-
-    def as_string(self):
-        result  = f'{self.error_name}: {self.details}\n'
-        result += f'File {self.pos_start.fn}, line {self.pos_start.ln + 1}'
-        return result
-
-class IllegalCharError(Error):
-    def __init__(self, pos_start, pos_end, details):
-        super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
 class Token:
     def __init__(self, type_, value=None):
@@ -64,57 +49,41 @@ class Lexer:
         else:
             return Token('FLOAT', num_str)
 
-
     def tokenize(self):
         tokens = []
 
         temp_str = ""
         while self.current_char != None:
-
-            if self.current_char == "\n":
-                self._move_next()
-                #print(temp_str)
-                #temp_str = ''
-
-
-            elif self.current_char == ' ':
-
+            if self.current_char == '\n' or self.current_char == ' ':
                 # This will recognise a variable and create a token for it
                 if temp_str in keywords.keys():
-                    tokens.append(Token(keywords[temp_str], temp_str))
-                    #tokens.append([keywords[temp_str], temp_str])
+                    tokens.append([keywords[temp_str], temp_str])
+                    self._move_next()
+                    temp_str = ""
+
+                #This will recognise a word and create an identifier token for it
+                elif re.match('[a-z]', temp_str) or re.match('[A-Z]', temp_str):
+                    tokens.append(['IDENTIFIER', temp_str])
                     self._move_next()
                     temp_str = ""
 
                 #This will recognise an integer and create an identifier token for it
                 elif re.match('[0-9]', temp_str):
-                    tokens.append(Token('INTEGER', temp_str))
+                    tokens.append(['INTEGER', temp_str])
                     self._move_next()
                     temp_str = ""
 
-                #elif temp_str in DIGITS:
-                    #tokens.append(Token(self.make_number()))
-                    #self._move_next()
-                    #temp_str = ''
-
                 #This will recognise operators
-                #elif temp_str in "%/*-+":
-                    #tokens.append(Token('OPERATOR', temp_str))
-                    #self._move_next()
-                    #temp_str = ""
-
-                #This will recognise a word and create an identifier token for it
-                else:
-                    tokens.append(Token('IDENTIFIER', temp_str))
+                elif temp_str in "%/*-+" and temp_str != ' ':
+                    tokens.append(['OPERATOR', temp_str])
                     self._move_next()
                     temp_str = ""
 
             else:
                 temp_str += self.current_char
                 self._move_next()
-
+        tokens.append([keywords[temp_str], temp_str])
         return tokens
-
 
 def nocomments(filename):
     filename = sys.argv[2]
@@ -138,7 +107,7 @@ def lex(filename):
 
     contents = nocomments(filename)
     print("Code without comments\n")
-    print(contents)
+    print(contents+'\n')
     lex = Lexer(contents)
     pp.pprint(lex.tokenize())
 
