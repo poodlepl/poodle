92ad8aba49eca1bc1f2dcbc0be007142cb474e74
Parth Krishna <thenerdsuperuser@gmail.com>
2019-04-25 16:44:26 +0530

    Token Type in symbol table function

--- modules/symbol.py
+++ modules/symbol.py
@@ -3,12 +3,14 @@ import sys
 sym_tab = {}
 
 
-def symbol_(identifier, position="", line="", value="-1",size=""):
+def symbol_(identifier, position="", line="", value="-1",size="",type_of=""):
     if identifier in sym_tab.keys() and sym_tab[identifier][0]==value:
         pass
     else:
         if size=="":
-            sym_tab[identifier] =[value, position, line, size]
+            sym_tab[identifier] =[value, position, line, size,type_of]
+        elif type_of=="":
+            sym_tab[identifier] =[value,sym_tab[identifier][1],sym_tab[identifier][2], size,sym_tab[identifier][4]]
         else:
-            sym_tab[identifier] =[value,sym_tab[identifier][1],sym_tab[identifier][2], size]
+            sym_tab[identifier] =[value,sym_tab[identifier][1],sym_tab[identifier][2], size,type_of]
     return sym_tab
